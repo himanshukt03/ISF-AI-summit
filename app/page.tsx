4c1ee24c36c3f59c5b5538136f2c410439d563ea
Himanshu <himanshushettykt03@gmail.com>
2025-02-21 21:31:43 +0530

    Speakers removed

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,6 @@ export default function Home() {
             <KeyHighlights />
             <MetaverseSection />
             <Features />
-            <Speakers />
             <Schedule />
             <WhyAttend />
             <Footer />
